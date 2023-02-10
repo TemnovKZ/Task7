8bf7efe469932d92fb6f51c6cbd7d7fdcfccc1ad
TemnovKZ <114981979+TemnovKZ@users.noreply.github.com>
2023-02-10 17:30:16 +0600

    789
    789

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
+
+/*--                Плагины                 --*/
+
+dayjs.extend(window.dayjs_plugin_timezone);
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_dayOfYear);
+dayjs.extend(window.dayjs_plugin_weekYear);
+dayjs.extend(window.dayjs_plugin_weekOfYear);
+
+/*--                Часы                 --*/
+
+
 let clock = document.querySelector('.time__clock');
 let wrapper = document.querySelector('.wrapper');
 let timeText = document.querySelector('.time__text');
 let hour = dayjs().format('HH');
-
 
 clock.innerHTML = dayjs().format('HH:mm');
 
 setInterval(function(){
     clock.innerHTML = dayjs().format('HH:mm');
     if (hour < 20 && hour > 7){
-        wrapper.style.backgroundImage = 'url(../img/bg-day.jpg)';
+        wrapper.style.backgroundImage = 'url(../img/bg-day.jpg)'
     } else {
-        wrapper.style.backgroundImage = 'url(../img/bg-night.jpg)';
+        wrapper.style.backgroundImage = 'url(../img/bg-night.jpg)'
     }
-}, 1000);
+}, 1000);   
 
 
 
 if (hour > 6 && hour <= 10) {
     document.querySelector('.time__icon').src='img/icon/sun.png'
-    timeText.innerHTML = 'GOOD MORNING, IT’S CURRENTLY'
-} else if (hour > 10 && hour < 16) {
-    timeText.innerHTML = 'GOOD DAY, IT’S CURRENTLY'
-} else if (hour > 16 && hour < 22) {
+    timeText.innerHTML = 'GOOD MORNING'
+} else if (hour > 10 && hour <= 17) {
+    document.querySelector('.time__icon').src='img/icon/sun.png'
+    timeText.innerHTML = 'GOOD DAY'
+} else if (hour > 17 && hour <= 22) {
     document.querySelector('.time__icon').src='img/icon/moon.png'
-    timeText.innerHTML = 'GOOD EVENING, IT’S CURRENTLY'
-} else if (hour > 22 || hour < 6) {
-    timeText.innerHTML = 'GOOD NIGHT, IT’S CURRENTLY'
+    timeText.innerHTML = 'GOOD EVENING'
+} else if (hour > 22 || hour <= 6) {
+    document.querySelector('.time__icon').src='img/icon/moon.png'
+    timeText.innerHTML = 'GOOD NIGHT'
 };
 
+
+/*--                Рандом                 --*/
 
 let randomClick = document.querySelector('.random__btn');
 
@@ -64,9 +79,40 @@ var autor = [
     'Михаил Булгаков',
     'Индира Ганди',
 ];
+
 function newQuote() {
     var randomNumber = Math.floor(Math.random() * (quotes.length));
     document.querySelector('.random__title').innerHTML = quotes[randomNumber];
     var randomAutor = Math.floor(Math.random() * (autor.length));
     document.querySelector('.random__subtitle').innerHTML = autor[randomNumber];
-}
+};
+
+
+/*--                Таймзона                 --*/
+
+let info = document.querySelector('.info');
+let random = document.querySelector('.random');
+let moreButton = document.querySelector('.time__button');
+let moreText = document.querySelector('.time__more');
+
+moreButton.addEventListener('click', function() {
+    info.classList.toggle('active');
+    random.classList.toggle('novis');
+    moreText.innerHTML = 'LESS';
+
+});
+
+let localCity = document.querySelector('.time__local');
+let utcTime = document.querySelector('.time__utc');
+let weekInfo = document.querySelector('.info__week');
+
+utcTime.innerHTML = dayjs().format("Z");
+localCity.innerHTML = dayjs.tz.guess();
+
+
+document.querySelector('.info__city').innerHTML = dayjs.tz.guess();
+document.querySelector('.info__day-week').innerHTML = dayjs().day();
+document.querySelector('.info__day-year').innerHTML = dayjs().dayOfYear();
+document.querySelector('.info__week').innerHTML = dayjs().week();
+
+console.log(dayjs().week());
